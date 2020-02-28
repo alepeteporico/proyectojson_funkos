c5b1cd1a3781c489f17451694c3016b876e4c56e
alepeteporico <tojandro@gmail.com>
2020-02-28 13:00:24 +0100

    Funcion 1 parte 2 terminada

--- funciones.py
+++ funciones.py
@@ -4,4 +4,8 @@ def ej1(datos):
         licencias.append(licencia)
     return licencias
 
-def ej1
+def ej1_2(datos,licencia):
+    figuras=[]
+    for figura in datos[licencia]:
+        figuras.append(figura["name"])
+    return figuras
