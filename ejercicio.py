c32fb4b5b3032ea71042e2fae44aefd4d863c46f
alepeteporico <tojandro@gmail.com>
2020-02-28 12:30:42 +0100

    menu terminado

--- ejercicio.py
+++ ejercicio.py
@@ -1,3 +1,14 @@
 import json
 with open ("products.json") as fichero:
     datos=json.load(fichero)
+
+while True:
+    print("=============================================MENÚ=============================================")
+    print("1. Listar las licencias que hay y al listarlas te pide si quieres listar las figuras que tiene alguna de ellas.")
+    print("2. Pide por teclado una licencia y te dice las figuras que tiene.")
+    print("3. Mostrar las figuras que sean de una categoría especfica.")
+    print("4. Pide por teclado una figura y te dice de que licencia es y si hay alguna relacionada (Del mismo personaje).")
+    print("5. Pide una figura y muestra en el navegador una imagen de la misma, después te pregunta si quieres ver las tiendas donde está disponible y te pregunta si quieres ir a alguna de ellas.")
+    print("6. Salir")
+    print("==============================================================================================")
+    opc=int(input("Elige una opción: "))
